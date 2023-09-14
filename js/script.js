29b8b238a3131220183ac1d578def328490ea6d2
Indita Pryatama P <42919037+inditapryatamap@users.noreply.github.com>
2023-09-14 09:24:37 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 function luas() {
     var s = document.getElementById('s').value;
     var luas = s * s;
+    var sisi1 = document.getElementsByClassName('sisi1');
+    var sisi2 = document.getElementsByClassName('sisi2');
+    sisi1[0].innerText = s;
+    sisi2[0].innerText = s;
     document.getElementById('luas').innerHTML = luas;
 }
 
 function keliling() {
     var s = document.getElementById("s").value;
     var keliling = 4 * s;
+    var sisi3 = document.getElementsByClassName("sisi3");
+    sisi3[0].innerText = s;
     document.getElementById("keliling").innerHTML = keliling;
 }
 
-function reset(){
+function reset() {
     document.getElementById("frm").reset();
 }
